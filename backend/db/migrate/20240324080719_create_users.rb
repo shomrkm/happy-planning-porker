72ba17ad1edc156f96e1167cee9741b34cbd0674
shomrkm <shochan1985@gmail.com>
2024-03-24 17:10:08 +0900

    feat: Add Users table

---
+++ backend/db/migrate/20240324080719_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string "provider", null: false
+      t.string "uid", null: false
+      t.string "name", null: false
+      t.string "email", null: false
+      t.string "image_url"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+  end
+end
